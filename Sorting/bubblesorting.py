164eeeac09b23d45702e3ca264979262325f43b5
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-07-21 20:49:09 +0000

    Jul 21, 2023, 9:49 PM

---
+++ Sorting/bubblesorting.py
@@ -0,0 +1,16 @@
+
+#bubblesorting
+
+def bubblessort(array):
+    array_len = len(array)
+
+    for i in range(0, array_len-1):
+    
+        for j in range(array_len-i-1):
+            if array[j] > array[j+1]:
+                print(f"j:{array[j]}, j+1:{array[j+1]}, arraylen:{array_len}")
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+print(bubblessort([2,6,1,5]))
+
